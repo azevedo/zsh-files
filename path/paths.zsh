f1098a250ae56ab4f0627fb1d0fdc8499311cb65
Bruno Azevedo <azevedo.252@gmail.com>
2017-12-08 18:53:28 +0000

    Adds more paths set by laptop

--- path/paths.zsh
+++ path/paths.zsh
@@ -1,15 +1,8 @@
-#
-# Prefixed Paths
-#
-
-# Ensure dotfiles bin directory is loaded first and as recommended by brew doctor
-export PATH="$HOME/.bin:/usr/local/bin:$PATH"
-
-
-#
-# Normal Paths
-#
 export JAVA_HOME="/Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home"
 export CHROME_BIN=/opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
+
+export PATH="$HOME/.bin:$PATH"
+# recommended by brew doctor
+export PATH="/usr/local/bin:$PATH"
 
 export PATH=$PATH:./node_modules/.bin:/usr/local/lib/node_modules
